33d1ce4d85c231e52a2847950f510793b26b311c
Dmitree <color.ivanov2016@yandex.ru>
2023-05-24 21:39:36 +0300

    init commit

---
+++ src/main/java/dmitreev/petproject/java/oneDayOneWay/OneDayOneWayApplication.java
@@ -0,0 +1,13 @@
+package dmitreev.petproject.java.oneDayOneWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDayOneWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneDayOneWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dmitreev/petproject/java/oneDayOneWay/OneDayOneWayApplicationTests.java
@@ -0,0 +1,13 @@
+package dmitreev.petproject.java.oneDayOneWay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneDayOneWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
