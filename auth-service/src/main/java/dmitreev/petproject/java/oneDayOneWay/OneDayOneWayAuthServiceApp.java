a55ec86d04d6645f4e92526becba630f5b3586a0
Dmitree <color.ivanov2016@yandex.ru>
2023-05-25 21:40:39 +0300

    add architecture project

---
+++ auth-service/src/main/java/dmitreev/petproject/java/oneDayOneWay/OneDayOneWayAuthServiceApp.java
@@ -0,0 +1,12 @@
+package dmitreev.petproject.java.oneDayOneWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneDayOneWayAuthServiceApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneDayOneWayAuthServiceApp.class, args);
+    }
+}
